a968eb719bf1892dcb39ea3cea91947714075031
gelbo <amb.gelb@yahoo.com>
2018-02-13 00:44:03 +0200

    Part 2 Add CarImage

--- Intro.py
+++ Intro.py
@@ -1,10 +1,25 @@
 import pygame
 
 pygame.init()
-gameDisplay = pygame.display.set_mode((800,600))
+display_width = 800
+display_height = 600
+
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+
+
+gameDisplay = pygame.display.set_mode((display_width,display_height))
 
 pygame.display.set_caption('A bit Racey')
 clock = pygame.time.Clock()
+
+carImg = pygame.image.load('car.png')
+
+def car(x,y):
+	gameDisplay.blit(carImg,(x,y))
+x = (display_width * 0.45)
+y = (display_height * 0.7)
 
 crashed = False
 
@@ -12,7 +27,9 @@ while not crashed:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			crashed = True
-		print(event)
+
+	gameDisplay.fill(white)
+	car(x,y)
 	pygame.display.update()
 
 	clock.tick(60)
