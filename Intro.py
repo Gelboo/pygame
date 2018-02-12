58da456fec9ac7ce14b9c7367ce2ea17164a8153
gelbo <amb.gelb@yahoo.com>
2018-02-13 01:44:21 +0200

    Part 4 Adding Boundaries

--- Intro.py
+++ Intro.py
@@ -7,7 +7,7 @@ display_height = 600
 black = (0,0,0)
 white = (255,255,255)
 red = (255,0,0)
-
+car_width = 128
 
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 
@@ -15,31 +15,36 @@ pygame.display.set_caption('A bit Racey')
 clock = pygame.time.Clock()
 
 carImg = pygame.image.load('car.png')
-
 def car(x,y):
 	gameDisplay.blit(carImg,(x,y))
-x = (display_width * 0.45)
-y = (display_height * 0.7)
-x_change = 0
-crashed = False
 
-while not crashed:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			crashed = True
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_LEFT:
-				x_change = -5
-			elif event.key == pygame.K_RIGHT:
-				x_change = 5
-		if event.type == pygame.KEYUP:
-			if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-				x_change = 0
-	x += x_change 
-	gameDisplay.fill(white)
-	car(x,y)
-	pygame.display.update()
-	#fram/second
-	clock.tick(60)
+def game_loop():
+	x = (display_width * 0.45)
+	y = (display_height * 0.7)
+	x_change = 0
+	gameExit = False
 
+	while not gameExit:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				gameExit = True
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_LEFT:
+					x_change = -5
+				elif event.key == pygame.K_RIGHT:
+					x_change = 5
+			if event.type == pygame.KEYUP:
+				if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+					x_change = 0
+		x += x_change 
+		gameDisplay.fill(white)
+		car(x,y)
+		# x is left top corner
+		if x > display_width-car_width or x < 0:
+			gameExit = True
+
+		pygame.display.update()
+		#fram/second
+		clock.tick(60)
+game_loop()
 pygame.quit()
