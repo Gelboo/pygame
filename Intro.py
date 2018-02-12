f7db7712e65b5292b11ca1bcabf5d7e81aa8d3cb
gelbo <amb.gelb@yahoo.com>
2018-02-13 01:00:16 +0200

    Part 3 Moving Image

--- Intro.py
+++ Intro.py
@@ -20,18 +20,26 @@ def car(x,y):
 	gameDisplay.blit(carImg,(x,y))
 x = (display_width * 0.45)
 y = (display_height * 0.7)
-
+x_change = 0
 crashed = False
 
 while not crashed:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			crashed = True
-
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_LEFT:
+				x_change = -5
+			elif event.key == pygame.K_RIGHT:
+				x_change = 5
+		if event.type == pygame.KEYUP:
+			if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+				x_change = 0
+	x += x_change 
 	gameDisplay.fill(white)
 	car(x,y)
 	pygame.display.update()
-
+	#fram/second
 	clock.tick(60)
 
 pygame.quit()
